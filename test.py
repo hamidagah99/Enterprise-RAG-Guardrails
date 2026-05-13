27dd6052ee31d40ba25bb8f2ee929232e26fd3fb
hamidagah99 <hamidagah99@gmail.com>
2026-05-13 22:00:29 +0200

    deleted some stuff

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from langchain_openai import ChatOpenAI
 
-# This connects directly to your LM Studio server
+
 llm = ChatOpenAI(
     base_url="http://127.0.0.1:1234/v1",
-    api_key="lm-studio", # It just needs a placeholder text here
+    api_key="lm-studio", 
     temperature=0.7
 )
 
